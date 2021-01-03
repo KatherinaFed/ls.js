97b6ecda9b4ac43328b2cde6e8571ab7f7a4278b
Ekaterina Fedoseeva <katarina1092@mail.ru>
2021-01-03 16:37:33 +0000

    #1

---
+++ script.js
@@ -0,0 +1,25 @@
+const ls = async (filepath) => {
+
+	const file = fs.stat(filepath).then((stats) => {
+		
+		if (stats.isFile()) {
+			return [{
+				filepath: filepath.split('..').pop(),
+				mode: stats.mode,
+			}];
+		} else {
+			return fs.readdir(filepath)
+				.then((filenames) => Promise.all(filenames
+					.map((name) => path.resolve(filepath, name))
+					.map((file) => fs.stat(file).then((stats) => {
+					return {
+						filepath: strip(file),
+						mode: stats.mode,
+					}
+				}
+			))))
+		}
+	});
+		const filesStats = await file;
+	return filesStats;
+}
